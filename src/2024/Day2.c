3dd291d78cea2aa7022fe8d5c3dccbae5384827c
JKachele <justin@kachele.com>
2024-12-04 16:58:00 -0500

    Completed Day 2

--- src/2024/Day2.c
+++ src/2024/Day2.c
@@ -12,6 +12,61 @@
 #include "../util/linkedlist.h"
 #include "../util/inputFile.h"
 
+bool reportSafe(int* report, int reportLen) {
+        int cur = report[0];
+        bool isSafe = 1;
+        bool isIncreasing = report[0] < report[1];
+        // printf("%d ", reports[i][0]);
+        for (int i = 1; i < reportLen; i++) {
+                if(report[i] == -1)
+                        break;
+
+                // Check if next value is different and within 3 
+                // of previous value
+                int diff = abs(cur - report[i]);
+                if (diff == 0 || diff > 3) {
+                        isSafe = false;
+                        break;
+                }
+                // Check if the report remains increasing or decreasing
+                if ((cur < report[i]) != isIncreasing) {
+                        isSafe = false;
+                        break;
+                }
+
+                cur = report[i];
+                // printf("%d ", reports[i][j]);
+        }
+        return isSafe;
+}
+
+// Part2: Brute Force - Iterating through each removal
+bool reportSafe2(int* report, int reportLen) {
+        // First try unaltered report
+        if(reportSafe(report, reportLen))
+                return true;
+
+        // If unaltered report is unsafe, remove an element and try again
+        for (int i = 0; i < reportLen; i++) {
+                int newReport[reportLen];
+                int oldIndex = 0;
+                int newIndex = 0;
+                do {
+                        if (oldIndex == i) {
+                                oldIndex++;
+                                continue;
+                        }
+                        newReport[newIndex] = report[oldIndex];
+                        oldIndex++;
+                        newIndex++;
+                } while (report[oldIndex - 1] != -1);
+
+                if(reportSafe(newReport, reportLen))
+                        return true;
+        }
+        return false;
+}
+
 void part1(llist *ll) {
         const int MAX_REPORT_LEN = 10;
 
@@ -36,51 +91,48 @@ void part1(llist *ll) {
                 current = current->next;
                 reportNum++;
         }
-        printf("\nPart 1: \n");
 
         int numSafe = 0;
         for (int i = 0; i < ll->length; i++) {
-                int cur = reports[i][0];
-                bool isSafe = 1;
-                bool isIncreasing = reports[i][0] < reports[i][1];
-                printf("%d ", reports[i][0]);
-                for (int j = 1; j < MAX_REPORT_LEN; j++) {
-                        if(reports[i][j] == -1)
-                                break;
-
-                        // Check if next value is different and within 3 
-                        // of previous value
-                        int diff = abs(cur - reports[i][j]);
-                        if (diff == 0 || diff > 3) {
-                                isSafe = false;
-                                break;
-                        }
-                        // Check if the report remains increasing or decreasing
-                        if ((cur < reports[i][j]) != isIncreasing) {
-                                isSafe = false;
-                                break;
-                        }
-
-                        cur = reports[i][j];
-                        printf("%d ", reports[i][j]);
-                }
-                if (isSafe == 1) {
-                        printf("Safe\n");
+                if(reportSafe(reports[i], MAX_REPORT_LEN))
                         numSafe++;
-                } else {
-                        printf("unsafe\n");
-                }
         }
 
         printf("Part1: Safe Reports: %d\n", numSafe);
 }
 
 void part2(llist *ll) {
+        const int MAX_REPORT_LEN = 10;
+
+        int reports[ll->length][MAX_REPORT_LEN];
+
+        int reportNum = 0;
         llNode *current = ll->head;
         while(current != NULL) {
+                char str[BUFFER_SIZE];
+                strncpy(str, (char*)current->data, BUFFER_SIZE);
+
+                char* entry = strtok(str, " ");
+                int entryNum = 0;
+                while (entry != NULL) {
+                        reports[reportNum][entryNum] = 
+                                strtol(entry, (char**)NULL, 10);
+                        entry = strtok(NULL, " ");
+                        entryNum++;
+                }
+                reports[reportNum][entryNum] = -1; // Set -1 as terminator
+
                 current = current->next;
+                reportNum++;
         }
-        printf("\nPart 2: \n");
+
+        int numSafe = 0;
+        for (int i = 0; i < ll->length; i++) {
+                if(reportSafe2(reports[i], MAX_REPORT_LEN))
+                        numSafe++;
+        }
+
+        printf("Part2: Safe Reports: %d\n", numSafe);
 }
 
 int main(int argc, char *argv[]) {
