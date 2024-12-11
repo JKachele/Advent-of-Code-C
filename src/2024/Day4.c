ee5b14c050861a6410029576c59621b20baebaa2
JKachele <justin@kachele.com>
2024-12-11 16:50:34 -0500

    Completed Day 4

--- src/2024/Day4.c
+++ src/2024/Day4.c
@@ -17,6 +17,17 @@ const int DIR_COUNT = 8;
 int SIZE_X = 0;
 int SIZE_Y = 0;
 
+typedef enum {
+        N,
+        NE,
+        E,
+        SE,
+        S,
+        SW,
+        W,
+        NW
+} dir_t;
+
 bool isValidPos(int x, int y) {
         return ((x >= 0) && (x < SIZE_X)
                         && (y >= 0) && (y < SIZE_Y));
@@ -24,64 +35,68 @@ bool isValidPos(int x, int y) {
 
 // Get next space based on current position and direction.
 // nextPos is of length 2 and is the output. -1 for invalis pos
-void getNextSpace(int nextPos[], int x, int y, int dir) {
+int getNextSpace(int nextPos[], int x, int y, dir_t dir) {
         nextPos[0] = -1;
         switch (dir) {
-        case 0: // North
+        case N: // North
                 if (!isValidPos(x, y - 1))
                         break;
                 nextPos[0] = x;
                 nextPos[1] = y - 1;
                 break;
-        case 1: // North-East
+        case NE: // North-East
                 if (!isValidPos(x + 1, y - 1))
                         break;
                 nextPos[0] = x + 1;
                 nextPos[1] = y - 1;
                 break;
-        case 2: // East
+        case E: // East
                 if (!isValidPos(x + 1, y))
                         break;
                 nextPos[0] = x + 1;
                 nextPos[1] = y;
                 break;
-        case 3: // South-East
+        case SE: // South-East
                 if (!isValidPos(x + 1, y + 1))
                         break;
                 nextPos[0] = x + 1;
                 nextPos[1] = y + 1;
                 break;
-        case 4: // South
+        case S: // South
                 if (!isValidPos(x, y + 1))
                         break;
                 nextPos[0] = x;
                 nextPos[1] = y + 1;
                 break;
-        case 5: // South-West
+        case SW: // South-West
                 if (!isValidPos(x - 1, y + 1))
                         break;
                 nextPos[0] = x - 1;
                 nextPos[1] = y + 1;
                 break;
-        case 6: // West
+        case W: // West
                 if (!isValidPos(x - 1, y))
                         break;
                 nextPos[0] = x - 1;
                 nextPos[1] = y;
                 break;
-        case 7: // North-West
+        case NW: // North-West
                 if (!isValidPos(x - 1, y - 1))
                         break;
                 nextPos[0] = x - 1;
                 nextPos[1] = y - 1;
                 break;
         default:
-                break;
+                return 2;
         }
+        return 0;
 }
 
 // dir: 0:N, 1:NE, 2:E, 3:SE, 4:S, 5:SW, 6:W, 7:NW
 int countXmas(char arr[][SIZE_X], int charNum, int x, int y, int dir) {
+        // Count instances of "XMAS" usind recursive calls looking for the
+        // next character in the word
+        int nextPos[2];
         switch (charNum) {
         case 0: // X
                 if (arr[y][x] != 'X')
@@ -89,7 +104,6 @@ int countXmas(char arr[][SIZE_X], int charNum, int x, int y, int dir) {
                 int count = 0;
                 // Loop through all 8 directions
                 for (int d = 0; d < DIR_COUNT; d++) {
-                        int nextPos[2];
                         getNextSpace(nextPos, x, y, d);
                         // Check if the next position is valid
                         if (nextPos[0] == -1)
@@ -97,17 +111,73 @@ int countXmas(char arr[][SIZE_X], int charNum, int x, int y, int dir) {
                         // recurse function with charNum = 1 to look for 'M'
                         count += countXmas(arr, 1, nextPos[0], nextPos[1], d);
                 }
-                break;
+                return count;
         case 1: // M
-                break;
+                if (arr[y][x] != 'M')
+                        break;
+                getNextSpace(nextPos, x, y, dir);
+                if (nextPos[0] == -1)
+                        break;
+                return countXmas(arr, 2, nextPos[0], nextPos[1], dir);
         case 2: // A
-                break;
+                if (arr[y][x] != 'A')
+                        break;
+                getNextSpace(nextPos, x, y, dir);
+                if (nextPos[0] == -1)
+                        break;
+                return countXmas(arr, 3, nextPos[0], nextPos[1], dir);
         case 3: // S
-                break;
+                if (arr[y][x] == 'S') {
+                        return 1;
+                } else {
+                        return 0;
+                }
         default:
+                fprintf(stderr, "Invalid charNum!");
                 break;
         }
         return 0;
+}
+
+int countMAS(char arr[][SIZE_X], int x, int y) {
+        if (arr[y][x] != 'A')
+                return 0;
+
+        int nw[2], se[2];
+        getNextSpace(nw, x, y, NW);
+        getNextSpace(se, x, y, SE);
+
+        if (nw[0] == -1 || se[0] == -1)
+                return 0;
+        
+        if (arr[nw[1]][nw[0]] == 'M') {
+                if (arr[se[1]][se[0]] != 'S')
+                        return 0;
+        } else if (arr[nw[1]][nw[0]] == 'S') {
+                if (arr[se[1]][se[0]] != 'M')
+                        return 0;
+        } else {
+                return 0;
+        }
+
+        int ne[2], sw[2];
+        getNextSpace(ne, x, y, NE);
+        getNextSpace(sw, x, y, SW);
+
+        if (ne[0] == -1 || sw[0] == -1)
+                return 0;
+        
+        if (arr[ne[1]][ne[0]] == 'M') {
+                if (arr[sw[1]][sw[0]] != 'S')
+                        return 0;
+        } else if (arr[ne[1]][ne[0]] == 'S') {
+                if (arr[sw[1]][sw[0]] != 'M')
+                        return 0;
+        } else {
+                return 0;
+        }
+
+        return 1;
 }
 
 void part1(llist *ll) {
@@ -126,25 +196,45 @@ void part1(llist *ll) {
                 lineNum++;
         }
 
-        // Search through array with a scaning 4x4 block looking for "xmas"
+        int xmasCount = 0;
+        for (int y = 0; y < SIZE_Y; y++) {
+                for (int x = 0; x < SIZE_X; x++) {
+                        xmasCount += countXmas(searchGrid, 0, x, y, 0);
+                }
+        }
 
-
-        printf("\nPart 1: \n");
+        printf("\nPart 1: XMAS Count: %d\n", xmasCount);
 }
 
 void part2(llist *ll) {
         llNode *current = ll->head;
+        SIZE_X = strlen((char*)current->data);
+        SIZE_Y = ll->length;
+        char searchGrid[SIZE_Y][SIZE_X];
+        int lineNum = 0;
         while(current != NULL) {
                 char str[BUFFER_SIZE];
                 strncpy(str, (char*)current->data, BUFFER_SIZE);
+                for (int i = 0; i < SIZE_X; i++) {
+                        searchGrid[lineNum][i] = str[i];
+                }
                 current = current->next;
+                lineNum++;
         }
-        printf("\nPart 2: \n");
+
+        int xmasCount = 0;
+        for (int y = 0; y < SIZE_Y; y++) {
+                for (int x = 0; x < SIZE_X; x++) {
+                        xmasCount += countMAS(searchGrid, x, y);
+                }
+        }
+
+        printf("\nPart 2: X-MAS Count: %d\n", xmasCount);
 }
 
 int main(int argc, char *argv[]) {
-        // llist *ll = getInputFile("assets/2024/Day4.txt");
-        llist *ll = getInputFile("assets/test.txt");
+        llist *ll = getInputFile("assets/2024/Day4.txt");
+        // llist *ll = getInputFile("assets/test.txt");
         llist_print(ll, printInput);
 
         part1(ll);
--- test.c
+++ test.c
@@ -7,65 +7,10 @@
  ************************************************/
 
 #include <stdio.h>
-#include <regex.h>
-#include <stdlib.h>
-#include <string.h>
 
-char* regexp(char *str, int *endPtr, regex_t regex) {
-        regmatch_t rm[1];
-        if (regexec(&regex, str, 1, rm, 0) != 0) {
-                // printf("No Regex Found\n");
-                return NULL;
-        }
-
-        // printf("Regex Found!\n");
-        *endPtr = (int)rm[0].rm_eo;
-
-        char* match = malloc(sizeof(char) * 20);
-        int matchLen = (int)(rm[0].rm_eo - rm[0].rm_so);
-
-        strncpy(match, str + rm[0].rm_so, matchLen);
-        return match;
-}
 
 int main(int argc, char *argv[]) {
         printf("Hello, World!\n");
-
-        char* str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-        // char* str = "A8B";
-        char* regexStr = "mul\\([[:digit:]]+,[[:digit:]]+\\)";
-        // char* regexStr = "A[0-9]+[[:alpha:]]+";
-
-
-        regex_t testRegex;
-        if (regcomp(&testRegex, regexStr, REG_EXTENDED) != 0) {
-                fprintf(stderr, "Regex Compilation Error\n");
-                return EXIT_FAILURE;
-        }
-
-        int *endPtr = malloc(sizeof *endPtr);
-        char *match = regexp(str, endPtr, testRegex);
-        printf("<<%s>>\n", str);
-        printf("match: %s, Remaining: %s\n", match, str + *endPtr);
-
-        if (strcmp(match, "mul(2,4)") == 0) {
-                printf("Match!\n");
-        }
-
-        // char* match = malloc(sizeof(char) * 20);
-        // regmatch_t rm[1];
-        // if (regexec(&testRegex, str, 1, rm, 0) == 0) {
-        //         printf("Regex Found!\n");
-        //         printf("<<%s>>\n", str);
-        //         printf("String: <<%.*s>>\n", (int)(rm[0].rm_eo - rm[0].rm_so), str + rm[0].rm_so);
-        //         int matchLen = (int)(rm[0].rm_eo - rm[0].rm_so);
-        //         strncpy(match, str + rm[0].rm_so, matchLen);
-        //         printf("Match: <<%s>>\n", match);
-        // } else {
-        //         printf("No Regex Found\n");
-        //         return 0;
-        // }
-
         return 0;
 }
 
