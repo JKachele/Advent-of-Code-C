8fd321eca145375525d7f5a4ac304e509261023a
JKachele <justin@kachele.com>
2025-02-11 15:40:21 -0500

    Completed Day 19

--- src/2024/Day19.c
+++ src/2024/Day19.c
@@ -8,41 +8,150 @@
 
 #include <stdio.h>
 #include <string.h>
-// #include <stdbool.h>
+#include <stdbool.h>
 #include "../util/linkedlist.h"
 #include "../util/inputFile.h"
-// #include "../lib/tllist.h"
+#include "../lib/tllist.h"
 // #include "../util/util.h"
 
+#define MAX_PATTERN_LEN 64
+
+typedef tll(char*) strtll;
+
+void printStrtll(strtll arr) {
+        tll_foreach(arr, it) {
+                printf("%s\n", it->item);
+        }
+}
+
+bool isPossible(char *goal, strtll patterns, int8_t memo[MAX_PATTERN_LEN]) {
+        int len = strlen(goal);
+        if (len == 0) return true;
+        if(memo[len] == -1) return false;
+        if(memo[len] == 1) return true;
+
+        tll_foreach(patterns, it) {
+                char *patt = it->item;
+                if (strlen(goal) < strlen(patt))
+                        continue;
+                if (memcmp(goal, patt, strlen(patt)) == 0) {
+                        if(isPossible(goal + strlen(patt), patterns, memo)) {
+                                memo[len] = 1;
+                                return true;
+                        }
+                }
+        }
+        memo[len] = -1;
+        return false;
+}
+
+int64_t possibilities(char *goal, strtll patterns, int64_t memo[MAX_PATTERN_LEN]) {
+        int len = strlen(goal);
+        if (len == 0) return 1;
+        if(memo[len] == -1) return 0;
+        if(memo[len] > 0) return memo[len];
+
+        int64_t numPossible = 0;
+        tll_foreach(patterns, it) {
+                char *patt = it->item;
+                if (strlen(goal) < strlen(patt))
+                        continue;
+                if (memcmp(goal, patt, strlen(patt)) == 0) {
+                        numPossible += possibilities(goal + strlen(patt), patterns, memo);
+                       
+                }
+        }
+        if (numPossible == 0)
+                memo[len] = -1;
+        else
+                memo[len] = numPossible;
+        return numPossible;
+}
+
 void part1(llist *ll) {
-        llNode *current = ll->head;
+        strtll patterns = tll_init();
+        char pattStr[BUFFER_SIZE];
+        strncpy(pattStr, (char*)ll->head->data, BUFFER_SIZE);
+        char *patt = strtok(pattStr, ",");
+        while (patt != NULL) {
+                if (patt[0] == ' ')
+                        patt++;
+                tll_push_back(patterns, patt);
+                patt = strtok(NULL, ",");
+        }
+        // printStrtll(patterns);
+
+        strtll goals = tll_init();
+        llNode *current = ll->head->next->next;
         while(current != NULL) {
-                char str[BUFFER_SIZE];
-                strncpy(str, (char*)current->data, BUFFER_SIZE);
+                char *in = (char*)current->data;
+                char *str = malloc(strlen(in) + 1);
+                strncpy(str, in, strlen(in) + 1);
+                tll_push_back(goals, str);
                 current = current->next;
         }
-        printf("Part 1: \n\n");
+        // printStrtll(goals);
+        
+        int possible = 0;
+        tll_foreach(goals, it) {
+                int8_t memo[MAX_PATTERN_LEN] = {0};
+                bool b = isPossible(it->item, patterns, memo);
+                // printf("%s - ", it->item);
+                // if (b)
+                //         printf("Possible\n");
+                // else
+                //         printf("Not Possible\n");
+                possible += b;
+        }
+
+        printf("Part 1: Num Possible: %d\n\n", possible);
+        tll_free_and_free(goals, free);
 }
 
 void part2(llist *ll) {
-        llNode *current = ll->head;
+        strtll patterns = tll_init();
+        char pattStr[BUFFER_SIZE];
+        strncpy(pattStr, (char*)ll->head->data, BUFFER_SIZE);
+        char *patt = strtok(pattStr, ",");
+        while (patt != NULL) {
+                if (patt[0] == ' ')
+                        patt++;
+                tll_push_back(patterns, patt);
+                patt = strtok(NULL, ",");
+        }
+        // printStrtll(patterns);
+
+        strtll goals = tll_init();
+        llNode *current = ll->head->next->next;
         while(current != NULL) {
-                char str[BUFFER_SIZE];
-                strncpy(str, (char*)current->data, BUFFER_SIZE);
+                char *in = (char*)current->data;
+                char *str = malloc(strlen(in) + 1);
+                strncpy(str, in, strlen(in) + 1);
+                tll_push_back(goals, str);
                 current = current->next;
         }
-        printf("Part 2: \n");
+        // printStrtll(goals);
+        
+        int64_t possible = 0;
+        tll_foreach(goals, it) {
+                int64_t memo[MAX_PATTERN_LEN] = {0};
+                int64_t n = possibilities(it->item, patterns, memo);
+                // printf("%s - %ld\n", it->item, n);
+                possible += n;
+        }
+
+        printf("Part 2: Num Possible: %ld\n\n", possible);
+        tll_free_and_free(goals, free);
 }
 
 int main(int argc, char *argv[]) {
         llist *ll;
         if (argc > 1 && strcmp(argv[1], "TEST") == 0) {
-                // ll = getInputFile("assets/test.txt");
                 ll = getInputFile("assets/tests/2024/Day19.txt");
         } else {
                 ll = getInputFile("assets/2024/Day19.txt");
         }
-        llist_print(ll, printInput);
+        // llist_print(ll, printInput);
 
         part1(ll);
         part2(ll);
--- test.c
+++ test.c
@@ -6,62 +6,26 @@
  *License-------GNU GPL-3.0
  ************************************************/
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include "src/lib/tllist.h"
 
-void printBin(unsigned long n, int bits) {
-        // Get number of bits needed
-        unsigned long i = n;
-        int numBits = 0;
-        while (i > 0) {
-                numBits++;
-                i >>= 1;
+bool startsWith(const char* a, const char *b) {
+        int len = strlen(a) < strlen(b) ? strlen(a) : strlen(b);
+        for (int i = 0; i < len; i++) {
+                if (a[i] != b[i])
+                        return false;
         }
-
-        if (bits != -1)
-                numBits = bits;
-        
-        unsigned long mask = 1 << (numBits - 1);
-        while (mask > 0) {
-                if ((n & mask) != 0)
-                        printf("1");
-                else
-                        printf("0");
-                mask >>= 1;
-        }
-        printf("\n");
-}
-
-void bin(uint64_t num) {
-        if (num > 1) {
-                bin(num / 2);
-        }
-        printf("%lu", num % 2);
-}
-
-void printBits(size_t const size, void const * const ptr) {
-        unsigned char *b = (unsigned char*) ptr;
-        unsigned char byte;
-        int i, j;
-
-        for (i = size - 1; i >= 0; i--) {
-                for (j = 7; j >= 0; j--) {
-                        byte = (b[i] >> j) & 1;
-                        printf("%u", byte);
-                }
-        }
-        printf("\n");
+        return true;
 }
 
 int main(int argc, char *argv[]) {
         printf("Hello, World!\n");
 
-        uint64_t num = 3033191740;
-        // printBin(3033191740, -1);
-        bin(3033191740);
-        printf("\n");
-        printBits(4, &num);
-        printf("%lu - %lu\n", sizeof(uint64_t), sizeof(long));
+        printf("%d\n", startsWith("Hello!!!", "He"));
+        printf("%lu\n", strlen("\0"));
 
         return 0;
 }
