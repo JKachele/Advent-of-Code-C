becf52a2b1997aa55dba9bd458c0f237c9e14cef
jkachele <justin@kachele.com>
2025-02-14 16:38:38 -0500

    Added Day 10 Solution

--- src/2024/Day10.c
+++ src/2024/Day10.c
@@ -21,23 +21,36 @@ typedef enum {
         RIGHT,
         DOWN,
         LEFT
-} direction;
+} dir;
 
-void printMap(int map[SIZE_Y][SIZE_X]) {
+typedef struct {
+        int elev;
+        bool visit;
+} node;
+
+void printMap(node map[SIZE_Y][SIZE_X]) {
         for (int y = 0; y < SIZE_Y; y++) {
                 for (int x = 0; x < SIZE_X; x++) {
-                        printf("%d", map[y][x]);
+                        printf("%d", map[y][x].elev);
                 }
                 printf("\n");
         }
 }
 
-bool isValidPos(vector2 pos) {
+void resetMap(node map[SIZE_Y][SIZE_X]) {
+        for (int y = 0; y < SIZE_Y; y++) {
+                for (int x = 0; x < SIZE_X; x++) {
+                        map[y][x].visit = false;
+                }
+        }
+}
+
+bool isValidPos(ivec2 pos) {
         return (pos.x >=0 && pos.x < SIZE_X
                         && pos.y >= 0 && pos.y < SIZE_Y);
 }
 
-int getNextPos(vector2 cur, vector2 *next, direction dir) {
+int getNextPos(ivec2 cur, ivec2 *next, dir dir) {
         switch (dir) {
         case UP:
                 next->x = cur.x;
@@ -50,28 +63,97 @@ int getNextPos(vector2 cur, vector2 *next, direction dir) {
                         return 1;
                 }
         case RIGHT:
+                next->x = cur.x + 1;
+                next->y = cur.y;
+                if (isValidPos(*next)) {
+                        return 0;
+                } else {
+                        next->x = -1;
+                        next->y = -1;
+                        return 1;
+                }
         case DOWN:
+                next->x = cur.x;
+                next->y = cur.y + 1;
+                if (isValidPos(*next)) {
+                        return 0;
+                } else {
+                        next->x = -1;
+                        next->y = -1;
+                        return 1;
+                }
         case LEFT:
+                next->x = cur.x - 1;
+                next->y = cur.y;
+                if (isValidPos(*next)) {
+                        return 0;
+                } else {
+                        next->x = -1;
+                        next->y = -1;
+                        return 1;
+                }
         default:
                 return -1;
         }
 }
 
-int findTrails(int map[SIZE_Y][SIZE_X], vector2 pos) {
-        if (map[pos.y][pos.x] == 9)
+int findTrails(node map[SIZE_Y][SIZE_X], ivec2 pos) {
+        if (map[pos.y][pos.x].visit)
+                return 0;
+
+        map[pos.y][pos.x].visit = true;
+
+        if (map[pos.y][pos.x].elev == 9) {
                 return 1;
+        }
 
-        int next = map[pos.y][pos.x] + 1;
-        vector2 nextpos = {-1, -1};
-        // Up
+        int trailCount = 0;
+        int next = map[pos.y][pos.x].elev + 1;
+        ivec2 nextpos = {-1, -1};
 
-        return 0;
+        if (getNextPos(pos, &nextpos, UP) == 0
+                        && map[nextpos.y][nextpos.x].elev == next)
+                trailCount += findTrails(map, nextpos);
+        if (getNextPos(pos, &nextpos, RIGHT) == 0
+                        && map[nextpos.y][nextpos.x].elev == next)
+                trailCount += findTrails(map, nextpos);
+        if (getNextPos(pos, &nextpos, DOWN) == 0
+                        && map[nextpos.y][nextpos.x].elev == next)
+                trailCount += findTrails(map, nextpos);
+        if (getNextPos(pos, &nextpos, LEFT) == 0
+                        && map[nextpos.y][nextpos.x].elev == next)
+                trailCount += findTrails(map, nextpos);
+        return trailCount;
+}
+
+int findTrails2(node map[SIZE_Y][SIZE_X], ivec2 pos) {
+        if (map[pos.y][pos.x].elev == 9) {
+                return 1;
+        }
+
+        int trailCount = 0;
+        int next = map[pos.y][pos.x].elev + 1;
+        ivec2 nextpos = {-1, -1};
+
+        if (getNextPos(pos, &nextpos, UP) == 0
+                        && map[nextpos.y][nextpos.x].elev == next)
+                trailCount += findTrails2(map, nextpos);
+        if (getNextPos(pos, &nextpos, RIGHT) == 0
+                        && map[nextpos.y][nextpos.x].elev == next)
+                trailCount += findTrails2(map, nextpos);
+        if (getNextPos(pos, &nextpos, DOWN) == 0
+                        && map[nextpos.y][nextpos.x].elev == next)
+                trailCount += findTrails2(map, nextpos);
+        if (getNextPos(pos, &nextpos, LEFT) == 0
+                        && map[nextpos.y][nextpos.x].elev == next)
+                trailCount += findTrails2(map, nextpos);
+        return trailCount;
 }
 
 void part1(llist *ll) {
         SIZE_Y = ll->length;
         SIZE_X = getLongestLine(ll);
-        int map[SIZE_Y][SIZE_X];
+        node map[SIZE_Y][SIZE_X];
 
         llNode *current = ll->head;
         int line = 0;
@@ -79,30 +161,61 @@ void part1(llist *ll) {
                 char str[BUFFER_SIZE];
                 strncpy(str, (char*)current->data, BUFFER_SIZE);
                 for (int x = 0; x < SIZE_X; x++) {
-                        map[line][x] = str[x] - '0';
+                        map[line][x].elev = str[x] - '0';
+                        map[line][x].visit = false;
                 }
                 current = current->next;
                 line++;
         }
         // printMap(map);
 
+        int totalTrails = 0;
         for (int y = 0; y < SIZE_Y; y++) {
                 for (int x = 0; x < SIZE_X; x++) {
-
+                        if (map[y][x].elev == 0) {
+                                int trails = findTrails(map, (ivec2){x, y});
+                                totalTrails += trails;
+                                // printf("(%d, %d): %d\n", x, y, trails);
+                                resetMap(map);
+                        }
                 }
         }
 
-        printf("Part 1: \n\n");
+        printf("Part 1: Total Trails: %d\n\n", totalTrails);
 }
 
 void part2(llist *ll) {
+        SIZE_Y = ll->length;
+        SIZE_X = getLongestLine(ll);
+        node map[SIZE_Y][SIZE_X];
+
         llNode *current = ll->head;
+        int line = 0;
         while(current != NULL) {
                 char str[BUFFER_SIZE];
                 strncpy(str, (char*)current->data, BUFFER_SIZE);
+                for (int x = 0; x < SIZE_X; x++) {
+                        map[line][x].elev = str[x] - '0';
+                        map[line][x].visit = false;
+                }
                 current = current->next;
+                line++;
         }
-        printf("Part 2: \n");
+        // printMap(map);
+
+        int totalTrails = 0;
+        for (int y = 0; y < SIZE_Y; y++) {
+                for (int x = 0; x < SIZE_X; x++) {
+                        if (map[y][x].elev == 0) {
+                                int trails = findTrails2(map, (ivec2){x, y});
+                                totalTrails += trails;
+                                // printf("(%d, %d): %d\n", x, y, trails);
+                                resetMap(map);
+                        }
+                }
+        }
+
+        printf("Part 2: Total Trails: %d\n\n", totalTrails);
 }
 
 int main(int argc, char *argv[]) {
