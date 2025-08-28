5e5fe8633ab60d264d7aa182e18f9102eb310df0
jkachele <justin@kachele.com>
2025-08-28 17:45:16 +0000

    Completed 2023 Day 14

--- src/2023/Day14.c
+++ src/2023/Day14.c
@@ -6,6 +6,7 @@
  *License-------GNU GPL-3.0
  ************************************************/
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -18,12 +19,8 @@
 #include "../util/util.h"
 
 #define INPUT_BUFFER_SIZE 4096
-
-typedef enum {
-        EMPTY,
-        SQUARE,
-        ROUND
-} rock;
+#define ROLLS 1000000000
+#define SEED 0x12345678
 
 static bool Debug = false;
 void debugP(const char *format, ...) {
@@ -34,43 +31,106 @@ void debugP(const char *format, ...) {
         va_end(args);
 }
 
-void printPlatform(ivec2 size, rock platform[size.y][size.x]) {
+// FNV Hashing Algorithm - H is seed
+uint32 FNV(const char *key, uint32 h) {
+        h ^= 2166136261UL;
+        const uint8 *data = (const uint8*)key;
+        for (int i=0; data[i]!='\0'; i++) {
+                h ^= data[i];
+                h *= 16777619;
+        }
+        return h;
+}
+
+void printPlatform(ivec2 size, char platform[size.y][size.x]) {
         if (!Debug) return;
         for (int y=0; y<size.y; y++) {
                 for (int x=0; x<size.x; x++) {
-                        if (platform[y][x] == EMPTY)
-                                printf(".");
-                        else if (platform[y][x] == SQUARE)
-                                printf("#");
-                        else if (platform[y][x] == ROUND)
-                                printf("O");
+                        printf("%c", platform[y][x]);
                 }
                 printf("\n");
         }
         printf("\n");
 }
 
-void rollPLatform(ivec2 size, rock platform[size.y][size.x]) {
-        for (int x=0; x<size.x; x++) {
-                int openSpace = 0;
-                for (int y=0; y<size.y; y++) {
-                        if (platform[y][x] == SQUARE)
-                                openSpace = y + 1;
-                        if (platform[y][x] == ROUND) {
-                                platform[y][x] = EMPTY;
-                                platform[openSpace][x] = ROUND;
-                                openSpace++;
+// dir: 0=UP, 1=LEFT, 2=DOWN, 3=RIGHT
+void tiltPLatform(ivec2 size, char platform[size.y][size.x], int8 dir) {
+        bool vert = (dir == 0 || dir == 2);
+        bool invert = (dir == 2 || dir == 3);
+
+        int sizeI = vert ? size.x : size.y;
+        int sizeJ = vert ? size.y : size.x;
+
+        for (int i=0; i<sizeI; i++) {
+                int openSpace = invert ? sizeJ - 1 : 0;
+                for (int j=0; j<sizeJ; j++) {
+                        int jInv = invert ? (sizeJ - 1) - j : j;
+                        int x = vert ? i : jInv;
+                        int y = vert ? jInv : i;
+
+                        if (platform[y][x] == '#')
+                                openSpace = invert ? jInv - 1 : jInv + 1;
+                        if (platform[y][x] == 'O') {
+                                platform[y][x] = '.';
+                                if (vert) 
+                                        platform[openSpace][x] = 'O';
+                                else
+                                        platform[y][openSpace] = 'O';
+                                if (invert)
+                                        openSpace--;
+                                else
+                                        openSpace++;
                         }
                 }
         }
 }
 
-int64 calcWeight(ivec2 size, rock platform[size.y][size.x]) {
+void rollPlatform(ivec2 size, char platform[size.y][size.x]) {
+        for (int i=0; i<4; i++) {
+                tiltPLatform(size, platform, i);
+        }
+}
+
+void spinCycle(ivec2 size, char platform[size.y][size.x]) {
+        char p[size.y+1][size.x];
+        memcpy(p, platform, (size.y+1) * size.x);
+
+        int32 *stateMap = calloc(UINT16_MAX+1, sizeof(int32));
+        // Only skip ahead after 2 consecutive matches (Avoid false positives)
+        bool found = false;
+        bool finish = false;
+        for (int i=1; i<=ROLLS; i++) {
+                rollPlatform(size, platform);
+                if (finish) continue;
+
+                uint16 hash = (uint16)FNV((char*)platform, SEED);
+                if (stateMap[hash] == 0) {
+                        stateMap[hash] = i;
+                        if (found) found = false;
+                        continue;
+                }
+
+                // If match found
+                if (!found) {
+                        found = true;
+                        continue;
+                }
+
+                // If 2 consecutive matches
+                int32 cycle = i - stateMap[hash];
+                int32 rollsLeft = ROLLS - i;
+                int32 cyclesLeft = rollsLeft / cycle;
+                i = i + (cycle * cyclesLeft);
+                finish = true;
+        }
+}
+
+int64 calcWeight(ivec2 size, char platform[size.y][size.x]) {
         int64 weight = 0;
         for (int i=1; i<=size.y; i++) {
                 int y = size.y - i;
                 for (int x=0; x<size.x; x++) {
-                        if (platform[y][x] == ROUND)
+                        if (platform[y][x] == 'O')
                                 weight += i;
                 }
         }
@@ -79,43 +139,49 @@ int64 calcWeight(ivec2 size, rock platform[size.y][size.x]) {
 
 void part1(llist *ll) {
         ivec2 size = {getLongestLine(ll), ll->length};
-        rock platform[size.y][size.x];
+        char platform[size.y][size.x];
 
         llNode *current = ll->head;
         int32 yind = 0;
         while(current != NULL) {
                 char *str = (char*)current->data;
 
-                for (int x=0; x<size.x; x++) {
-                        if (str[x] == '.')
-                                platform[yind][x] = EMPTY;
-                        else if (str[x] == '#')
-                                platform[yind][x] = SQUARE;
-                        else if (str[x] == 'O')
-                                platform[yind][x] = ROUND;
-                        else
-                                printf("%c at (%d, %d)\n", str[x], x, yind);
-                }       
+                strncpy(platform[yind], str, size.x);
 
                 current = current->next;
                 yind++;
         }
         // printPlatform(size, platform);
-        rollPLatform(size, platform);
+        tiltPLatform(size, platform, 0);
         // printPlatform(size, platform);
+
         int64 weight = calcWeight(size, platform);
 
         printf("Part 1: %ld\n\n", weight);
 }
 
 void part2(llist *ll) {
+        ivec2 size = {getLongestLine(ll), ll->length};
+        char platform[size.y+1][size.x]; // +1 to add null terminator for FNV
+        platform[size.y][0] = '\0'; // null terminator for FNV
+
         llNode *current = ll->head;
+        int32 yind = 0;
         while(current != NULL) {
-                char str[INPUT_BUFFER_SIZE];
-                strncpy(str, (char*)current->data, INPUT_BUFFER_SIZE);
+                char *str = (char*)current->data;
+
+                strncpy(platform[yind], str, size.x);
+
                 current = current->next;
+                yind++;
         }
-        printf("Part 2: \n");
+        // printPlatform(size, platform);
+
+        spinCycle(size, platform);
+
+        int64 weight = calcWeight(size, platform);
+
+        printf("Part 2: %ld\n\n", weight);
 }
 
 int main(int argc, char *argv[]) {
--- src/test.c
+++ src/test.c
@@ -9,15 +9,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "util/util.h"
+#include "util/vector.h"
+
+void tilt(ivec2 size, int arr[size.y][size.x], int8 dir) {
+        bool vert = (dir == 0 || dir == 2);
+        bool invert = (dir == 2 || dir == 3);
+
+        int sizeI = vert ? size.x : size.y;
+        int sizeJ = vert ? size.y : size.x;
+
+        for (int i=0; i<sizeI; i++) {
+                for (int j=0; j<sizeJ; j++) {
+                        int jInv = invert ? (sizeJ - 1) - j : j;
+
+                        int x = vert ? i : jInv;
+                        int y = vert ? jInv : i;
+                        printf("%d ", arr[y][x]);
+                }
+                // printf("\n");
+        }
+        printf("\n");
+}
 
 int main(int argc, char *argv[]) {
         printf("Hello, World!\n");
 
-        int *test = calloc(10, sizeof(int));
-        test[3] = 10;
-        printf("%d\n", test[3]);
-        printf("%d\n", *(test + 3));
+        int arr[4][3] = {
+                {0, 1,  2},
+                {3, 4,  5},
+                {6, 7,  8},
+                {9, 10, 11}
+        };
 
-        return 0;
+        ivec2 size = {3, 4};
+
+        printf("UP: ");
+        tilt(size, arr, 0);
+        printf("LEFT: ");
+        tilt(size, arr, 1);
+        printf("DOWN: ");
+        tilt(size, arr, 2);
+        printf("RIGHT: ");
+        tilt(size, arr, 3);
 }
 
