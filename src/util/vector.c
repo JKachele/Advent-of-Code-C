707c58b8dc568edc377f25448b892f2c87387ca1
jkachele <justin@kachele.com>
2025-09-05 17:38:00 +0000

    Added Linux int types and updated vector to allow indexed access

--- src/util/vector.c
+++ src/util/vector.c
@@ -16,6 +16,13 @@ ivec2 addIVec2(ivec2 a, ivec2 b) {
         return sum;
 }
 
+ivec2 ivec2Add(ivec2 a, ivec2 b) {
+        ivec2 sum;
+        sum.x = a.x + b.x;
+        sum.y = a.y + b.y;
+        return sum;
+}
+
 lvec2 addLVec2(lvec2 a, lvec2 b) {
         lvec2 sum;
         sum.x = a.x + b.x;
@@ -23,7 +30,22 @@ lvec2 addLVec2(lvec2 a, lvec2 b) {
         return sum;
 }
 
+lvec2 lvec2Add(lvec2 a, lvec2 b) {
+        lvec2 sum;
+        sum.x = a.x + b.x;
+        sum.y = a.y + b.y;
+        return sum;
+}
+
 ivec3 addIVec3(ivec3 a, ivec3 b) {
+        ivec3 sum;
+        sum.x = a.x + b.x;
+        sum.y = a.y + b.y;
+        sum.z = a.z + b.z;
+        return sum;
+}
+
+ivec3 ivec3Add(ivec3 a, ivec3 b) {
         ivec3 sum;
         sum.x = a.x + b.x;
         sum.y = a.y + b.y;
@@ -39,12 +61,28 @@ lvec3 addLVec3(lvec3 a, lvec3 b) {
         return sum;
 }
 
+lvec3 lvec3Add(lvec3 a, lvec3 b) {
+        lvec3 sum;
+        sum.x = a.x + b.x;
+        sum.y = a.y + b.y;
+        sum.z = a.z + b.z;
+        return sum;
+}
+
 bool ivec2Eq(ivec2 a, ivec2 b) {
         return ((a.x == b.x) && (a.y == b.y));
 }
 
 bool lvec2Eq(lvec2 a, lvec2 b) {
         return ((a.x == b.x) && (a.y == b.y));
+}
+
+bool ivec3Eq(ivec3 a, ivec3 b) {
+        return ((a.x == b.x) && (a.y == b.y) && (a.z == b.z));
+}
+
+bool lvec3Eq(lvec3 a, lvec3 b) {
+        return ((a.x == b.x) && (a.y == b.y) && (a.z == b.z));
 }
 
 int32_t ivec2area(ivec2 v) {
--- src/util/vector.h
+++ src/util/vector.h
@@ -11,34 +11,72 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef struct {
-        int64_t x;
-        int64_t y;
-} lvec2;
-
-typedef struct {
-        int32_t x;
-        int32_t y;
+typedef union ivec2 {
+        int32_t raw[2];
+        struct {
+                int32_t x;
+                int32_t y;
+        };
 } ivec2;
 
-typedef struct {
-        int64_t x;
-        int64_t y;
-        int64_t z;
-} lvec3;
-
-typedef struct {
-        int32_t x;
-        int32_t y;
-        int32_t z;
+typedef union ivec3 {
+        int32_t raw[3];
+        struct {
+                int32_t x;
+                int32_t y;
+                int32_t z;
+        };
 } ivec3;
 
+typedef union ivec4 {
+        int32_t raw[4];
+        struct {
+                int32_t x;
+                int32_t y;
+                int32_t z;
+                int32_t w;
+        };
+} ivec4;
+
+typedef union lvec2 {
+        int64_t raw[2];
+        struct {
+                int64_t x;
+                int64_t y;
+        };
+} lvec2;
+
+typedef union lvec3 {
+        int64_t raw[3];
+        struct {
+                int64_t x;
+                int64_t y;
+                int64_t z;
+        };
+} lvec3;
+
+typedef union lvec4 {
+        int64_t raw[4];
+        struct {
+                int64_t x;
+                int64_t y;
+                int64_t z;
+                int64_t w;
+        };
+} lvec4;
+
 ivec2 addIVec2(ivec2 a, ivec2 b);
+ivec2 ivec2Add(ivec2 a, ivec2 b);
 lvec2 addLVec2(lvec2 a, lvec2 b);
+lvec2 lvec2Add(lvec2 a, lvec2 b);
 ivec3 addIVec3(ivec3 a, ivec3 b);
+ivec3 ivec3Add(ivec3 a, ivec3 b);
 lvec3 addLVec3(lvec3 a, lvec3 b);
+lvec3 lvec3Add(lvec3 a, lvec3 b);
 bool ivec2Eq(ivec2 a, ivec2 b);
 bool lvec2Eq(lvec2 a, lvec2 b);
+bool ivec3Eq(ivec3 a, ivec3 b);
+bool lvec3Eq(lvec3 a, lvec3 b);
 int32_t ivec2area(ivec2 v);
 int64_t lvec2area(lvec2 v);
 
